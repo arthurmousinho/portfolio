2d9c192a9c1721564423c15f15d9d38fb9e2df26
arthurmousinho <arthursilvamo296@gmail.com>
2025-03-08 12:22:14 -0300

    feat: adds DotPattern component for animated background effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@ import { Experiences } from "@/app/(sections)/experiences";
 import { Hero } from "@/app/(sections)/hero";
 import { Skills } from "@/app/(sections)/skills";
 import { Projects } from "./(sections)/projects";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div className="w-[90vw] max-w-[670px] mx-auto mt-10 space-y-10 mb-[300px]">
+      <DotPattern
+        width={20}
+        height={20}
+        cx={1}
+        cy={1}
+        cr={1}
+        className={cn(
+          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
+        )}
+      />
       <Hero />
       <Skills />
       <Experiences />
